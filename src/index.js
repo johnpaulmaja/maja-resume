b46cdbfe5cd6bea005b40ea6a5d290549d21f1ba
jharapsody <johnpaulmaja5@gmail.com>
2024-07-25 18:26:15 +0800

    0.2.5

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'; // Importing the CSS
 import App from './App';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 ReactDOM.render(
   <React.StrictMode>
